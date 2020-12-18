0d751bd2e0de6697b2b50ffd8863acbcd9867338
NoMoreBuild <woolimlee.dev@gmail.com>
2020-12-18 16:16:55 +0900

    update:새로운 품절목록 라우터 추가

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import React from "react";
 const SoldOutReport = React.lazy(() => import("./views/soldout/SoldOutReport"));
 const SoldOut = React.lazy(() => import("./views/soldout/SoldOut"));
+const SoldOutNew = React.lazy(() => import("./views/soldout/SoldOutNew"));
 const Detail = React.lazy(() => import("./views/soldout/detail"));
 
 const routes = [
   { path: "/", exact: true, name: "Home" },
   { path: "/soldout", exact: true, name: "SoldOut", component: SoldOut },
+  {
+    path: "/soldout_new",
+    name: "SoldOutNew",
+    component: SoldOutNew,
+  },
   { path: "/soldout/report", name: "SoldOutReport", component: SoldOutReport },
   { path: "/soldout/:idx/:order_no", name: "Detail", component: Detail },
 ];
